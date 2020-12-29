dd8bec5fcb870a43fc6a7f9ea6e7f8327fb69441
OskarJot <oskar.jarnot@gmail.com>
2020-12-29 16:59:21 +0100

    API - how work

--- 01/index.js
+++ 01/index.js
@@ -1,2 +1,36 @@
-// https://api.punkapi.com/v2/beers
+const API_URL = 'https://api.punkapi.com/v2/beers';
+const container = document.querySelector('.container'); 
 
+const render = (data) => {
+    if (!data.length) return;
+    const fragment = document.createDocumentFragment();
+    data.forEach(({ name, tagline, description, image_url: imageURL }) => {
+        const div = document.createElement('div');
+        div.classList.add('beer');
+        div.innerHTML = `
+        <div class="beer--content">
+            <h1 class="beer--title">${name}</h1>
+            <p class="beer--tagline">${tagline}</p>
+            <p class="beer--description">${description}</p>
+        </div>
+        <img class="beer--image" src="${imageURL}">
+        `;
+        fragment.appendChild(div);
+    });
+
+    container.appendChild(fragment);
+}
+
+const success = (data) => {
+    const beers = JSON.parse(data.target.responseText);
+    render(beers);
+}
+const error = (err) => {
+    console.log(err);
+}
+
+const req = new XMLHttpRequest();
+req.onload = success;
+req.onerror = error;
+req.open('GET', API_URL);
+req.send();
