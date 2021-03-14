b0c1e7b05ad997fac94a7bfb6f15d4dca8bedb94
Kamil Szerląg <kamilszerlag@gmail.com>
2021-03-14 17:46:40 +0100

    solution

--- 02/studdybuddy-front/src/components/StudentForm/StudentForm.js
+++ 02/studdybuddy-front/src/components/StudentForm/StudentForm.js
@@ -7,84 +7,124 @@ import { getGradesAvg } from 'helpers/gradesAvg';
 import * as yup from 'yup';
 import { Title, Wrapper } from 'components/SchoolNews/SchoolNews.styles';
 import {
-  AddScoreWrapper,
-  Field,
-  Form,
-  Input,
-  Label, SaveButton,
-  Scores,
-  ScoresWrapper
+	AddScoreWrapper,
+	Field,
+	Form,
+	Input,
+	Label,
+	SaveButton,
+	Scores,
+	ScoresWrapper,
+	Error,
 } from 'components/StudentForm/StudentForm.styles';
 import { api, endpoints } from 'api';
 
 let grade = yup.number();
 grade
-  .integer('Ocena musi być liczbowa')
-  .positive('Ocena musi być liczbą dodatnią')
-  .min(1, 'Ocena musi być wyższa od 0')
-  .max(6, 'Ocena musi być niższa od 7');
+	.integer('Ocena musi być liczbowa')
+	.positive('Ocena musi być liczbą dodatnią')
+	.min(1, 'Ocena musi być wyższa od 0')
+	.max(6, 'Ocena musi być niższa od 7');
 
 const StudentForm = () => {
-  const [scores, setScores] = useState([]);
-  const [scoreInputValue, setScoreInputValue] = useState('');
-  const { register, handleSubmit, watch, errors } = useForm();
-  const [average, setAverage] = useState();
+	const [scores, setScores] = useState([]);
+	const [scoreInputValue, setScoreInputValue] = useState('');
+	const { register, handleSubmit, watch, errors } = useForm();
+	const [average, setAverage] = useState();
 
-  const onSubmit = ({ name, age, group }) => {
-    api.post(endpoints.users, {
-      name,
-      group,
-      age: parseInt(age, 10),
-      grades: scores,
-    })
-      .then(response => {
-        console.log(response);
-      })
-      .catch( error => {
-        console.log(error);
-      })
-  };
+	const onSubmit = ({ name, age, group }, e) => {
+		//zamiast całego obiektu musimy dokonać destrukturyzacji żeby age zamienić na number
+		api.post(endpoints.users, {
+			name,
+			group,
+			age: parseInt(age, 10),
+			grades: scores,
+		})
+			.then((response) => {
+				console.log(response);
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+		e.target.reset();
+		setScores([]);
+	};
 
-  useEffect(() => {
-    setAverage(getGradesAvg(scores));
-  }, [scores]);
+	useEffect(() => {
+		setAverage(getGradesAvg(scores));
+	}, [scores]);
 
-  const handleScoreInputChange = (e) => {
-    setScoreInputValue(e.target.value);
-  };
+	const handleScoreInputChange = (e) => {
+		setScoreInputValue(e.target.value);
+	};
 
-  const handleAddGrade = () => {
-    setScores([...scores, parseInt(scoreInputValue)]);
-    setScoreInputValue('');
-  };
+	const handleAddGrade = () => {
+		if (scoreInputValue) {
+			setScores([...scores, parseInt(scoreInputValue)]);
+		}
+		setScoreInputValue('');
+	};
 
-  return (
-    <Wrapper>
-      <Title>Dodaj ucznia</Title>
-      <Form onSubmit={handleSubmit(onSubmit)}>
-        <Field><Label>Imię</Label>
-          <Input id="name" name="name" ref={register}/></Field>
-        <Field><Label>Grupa</Label>
-          <Input id="group" name="group" ref={register}/></Field>
+	return (
+		<Wrapper>
+			<Title>Dodaj ucznia</Title>
+			<Form onSubmit={handleSubmit(onSubmit)}>
+				<Field>
+					<Label>Imię</Label>
+					<Input
+						id="name"
+						name="name"
+						ref={register({ required: true })}
+					/>
+					{errors.name && <Error>Podaj imię</Error>}
+				</Field>
+				<Field>
+					<Label>Grupa</Label>
+					<Input
+						id="group"
+						name="group"
+						ref={register({ required: true })}
+					/>
+					{errors.group && <Error>Podaj grupę</Error>}
+				</Field>
 
-        <Field><Label>Wiek</Label>
-          <Input id="age" name="age" ref={register}/></Field>
-        <Field><Label>Dodaj ocenę</Label>
-          <AddScoreWrapper>
-            <Input id="score" name="score" value={scoreInputValue} onChange={handleScoreInputChange}/>
-            <EditButton type="button" onClick={handleAddGrade}>+</EditButton>
-          </AddScoreWrapper></Field>
-        <ScoresWrapper>
-          <Scores>
-            <p>Oceny</p>
-            <p>{scores.join(', ')}</p>
-          </Scores>
-          <Average value={average}>{average}</Average>
-        </ScoresWrapper>
-        <SaveButton type="submit">zapisz</SaveButton>
-      </Form>
-    </Wrapper>
-  );
+				<Field>
+					<Label>Wiek</Label>
+					<Input
+						type="number"
+						id="age"
+						name="age"
+						ref={register({ required: true })}
+					/>
+					{errors.age && <Error>Podaj wiek</Error>}
+				</Field>
+				<Field>
+					<Label>Dodaj ocenę</Label>
+					<AddScoreWrapper>
+						<Input
+							type="number"
+							id="score"
+							name="score"
+							value={scoreInputValue}
+							onChange={handleScoreInputChange}
+							ref={register}
+						/>
+						<EditButton type="button" onClick={handleAddGrade}>
+							+
+						</EditButton>
+					</AddScoreWrapper>
+				</Field>
+				<ScoresWrapper>
+					<Scores>
+						<p>Oceny</p>
+						<p>{scores.join(', ')}</p>
+					</Scores>
+					<Average value={average}>{average}</Average>
+				</ScoresWrapper>
+				<SaveButton type="submit">zapisz</SaveButton>
+			</Form>
+		</Wrapper>
+	);
 };
 
 StudentForm.propTypes = {};
--- 02/studdybuddy-front/src/components/StudentsList/StudentsList.js
+++ 02/studdybuddy-front/src/components/StudentsList/StudentsList.js
@@ -4,27 +4,30 @@ import { Wrapper } from 'components/SchoolNews/SchoolNews.styles';
 import { api, endpoints } from 'api';
 
 const StudentsList = () => {
-  const [students, setStudents] = useState([]);
+	const [students, setStudents] = useState([]);
 
-  useEffect(() => {
-    api.get(endpoints.users)
-      .then(({ data }) => {
-        setStudents(data)
-        console.log(data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, [])
+	useEffect(() => {
+		api.get(endpoints.users)
+			.then(({ data }) => {
+				setStudents(data);
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	}, []);
 
-  return (
-    <Wrapper>
-      { students.length
-        ? students.map(student => <StudentProfile key={student._id} studentData={student} />)
-        : <h2>No students available</h2> }
-    </Wrapper>
-  );
-}
+	return (
+		<Wrapper>
+			{students.length ? (
+				students.map((student) => (
+					<StudentProfile key={student._id} studentData={student} />
+				))
+			) : (
+				<h2>No students available</h2>
+			)}
+		</Wrapper>
+	);
+};
 
 StudentsList.propTypes = {};
 
